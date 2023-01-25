96654d3b22a9a51c2ecfd8f2561b1f7770da2396
Will Hedges <wshedges@outlook.com>
2023-01-25 11:55:58 -0600

    added 'top priced' button, logic

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -15,6 +15,8 @@ const sortProductNamesByAlpha = (product1, product2) => {
 
 export const Products = () => {
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [topPriced, setTopPriced] = useState(false);
 
   // fetch all products
   useEffect(() => {
@@ -23,18 +25,44 @@ export const Products = () => {
       .then((productsArray) => {
         productsArray.sort(sortProductNamesByAlpha);
         setProducts(productsArray);
+        setFilteredProducts(productsArray);
       });
   }, []);
 
+  useEffect(() => {
+    if (topPriced) {
+      const expensiveProducts = products.filter((p) => p.unitPrice > 8);
+      setFilteredProducts(expensiveProducts);
+    } else {
+      setFilteredProducts(products);
+    }
+    // I HATE THIS WARNING
+  }, [topPriced]);
+
   return (
-    <ul className="products__list">
-      {products.map((product) => {
-        return (
-          <li className="product">
-            {product.name} (${product.unitPrice}/ea)
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <button
+        className="btn"
+        onClick={() => {
+          if (topPriced) {
+            setTopPriced(false);
+          } else {
+            setTopPriced(true);
+          }
+        }}
+      >
+        Top Priced
+      </button>
+      <ul className="products__list">
+        {filteredProducts.map((product) => {
+          return (
+            <li className="product">
+              {/* TODO FIXME want to preserve trailing zeros */}
+              {product.name} (${product.unitPrice}/ea)
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
